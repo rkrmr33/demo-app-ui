208f822d084423d219daa842a3cde7d10397e129
rkrmr33 <roikramer120@gmail.com>
2021-11-30 00:09:49 +0200

    initial commit

--- hack/docker.build.js
+++ hack/docker.build.js
@@ -3,6 +3,6 @@ const { exec } = require('child_process');
 
 const packagejson = require('../package.json');
 
-const proc = exec(`docker build . -t quay.io/roikramer120/demo-app-${packagejson.name}:${packagejson.version}`);
+const proc = exec(`docker build . -t quay.io/roikramer120/${packagejson.name}:${packagejson.version}`);
 proc.stdout.on('data', console.log.bind(console));
 proc.stderr.on('data', console.error.bind(console));
